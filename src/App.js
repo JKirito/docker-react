7e1415c0dcec8b792e60980fec20725e321a1900
JKirito <byrxarpitsingh@gmail.com>
2021-11-20 03:07:00 +0530

    added all data commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>I am inside Docker</h1>
+      <h3>See realtime update</h3>
+    </div>
+  );
+}
+
+export default App;
